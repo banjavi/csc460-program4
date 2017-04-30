59b345b8d2441e3c8691c351c5c97063c6e61cbb
Colin Widner <cwidner@email.arizona.edu>
2017-04-30 14:37:17 -0700

    Update DatabaseController.java

--- ROOT/WEB-INF/classes/dbController/DatabaseController.java
+++ ROOT/WEB-INF/classes/dbController/DatabaseController.java
@@ -136,7 +136,7 @@ public class DatabaseController {
 							"##" + rs.getInt("USER_ID") + "##" + rs.getDate("DATE_PLACED") + "##" +
 							rs.getDate("PICK_UP_DATE") + "##" + rs.getInt("PRODUCT_ID") + "##" +
 							rs.getInt("QUANTITY");
-					result_order.add(temp_record);
+					result_orders.add(temp_record);
 				}
 				return result_orders
 			} catch (SQLException sqlex) {
