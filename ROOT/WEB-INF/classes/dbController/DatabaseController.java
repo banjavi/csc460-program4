ff1c6cf6c61464ce66eed0b31aca62a2da616f20
Colin Widner <cwidner@email.arizona.edu>
2017-04-30 18:29:20 -0700

    Update DatabaseController.java

--- ROOT/WEB-INF/classes/dbController/DatabaseController.java
+++ ROOT/WEB-INF/classes/dbController/DatabaseController.java
@@ -171,26 +171,30 @@ public class DatabaseController {
 			}
 		return null;
 	}
+	*/
 	
-	public Vector<String> viewUnprocessedOrders() {
-		String sql_query = "SELECT * FROM banjavi.orders WHERE pick_up_date= NULL";
-		try{
-			ResultSet rs = statement_.executeQuery(sql_query);
-			Vector<String> result_orders = new Vector<String>();
-			while(rs.next()) {
-				String temp_record = rs.getInt("PURCHASE_ID") + "##" + rs.getInt("ORDER_ID") +
-						"##" + rs.getInt("USER_ID") + "##" + rs.getDate("DATE_PLACED") + "##" +
-						rs.getDate("PICK_UP_DATE") + "##" + rs.getInt("PRODUCT_ID") + "##" +
-						rs.getInt("QUANTITY");
-				result_order.add(temp_record);
-			}
-			return result_orders
-			} catch (SQLException sqlex) {
-				sqlex.printStackTrace();
-			}
-		return null;
-	}	
-*/
+	/*
+	 * Returns all customer orders that have not been checked out yet
+	 */
+public Vector<String> viewUnprocessedOrders() {
+	java.sql.Date d = new java.sql.Date(0, 0, 0);
+	String sql_query = "SELECT * FROM banjavi.orders WHERE pick_up_date= TO_DATE('" + d + "', 'yyyy-mm-dd'), ";
+	try{
+		ResultSet rs = statement_.executeQuery(sql_query);
+		Vector<String> result_orders = new Vector<String>();
+		while(rs.next()) {
+			String temp_record = rs.getInt("PURCHASE_ID") + "##" + rs.getInt("ORDER_ID") +
+					"##" + rs.getInt("USER_ID") + "##" + rs.getDate("DATE_PLACED") + "##" +
+					rs.getDate("PICK_UP_DATE") + "##" + rs.getInt("PRODUCT_ID") + "##" +
+					rs.getInt("QUANTITY");
+			result_orders.add(temp_record);
+		}
+		return result_orders;
+		} catch (SQLException sqlex) {
+			sqlex.printStackTrace();
+		}
+	return null;
+	}
 	public boolean insertUser (String username, String password, String type) {
 
 
