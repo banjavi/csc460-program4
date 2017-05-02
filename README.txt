45e28616f14de59d528a36f51ae3aa2e73112c71
banjavi <banjavi96@gmail.com>
2017-05-02 00:26:37 -0700

    Write readMe
    Add documentation
    
    Update README.txt
    
    Update README.txt
    
    Update README.txt
    
    Update README.txt and documentation.

--- README.txt
+++ README.txt
@@ -4,3 +4,33 @@ Authors: Alex, Bijan, Colin, Monica,
 
 A ReadMe.txt describing how the TA can operate your website to see the required functionalities,
 and the work-load distribution among team members (that is, who was responsible for what?).
+
+
+How To Operate
+-----------------
+To test the required functionalities (insertion, deletion, update), go to
+the User Settings page while logged in as a Manager. Adding users (make sure the
+category is typed in as either "Manager", "Employee", or "Customer" exactly), updating users,
+and deleting users are all implemented. There are many other insertions, deletions, and updates
+in the application, but this is the simplest way to see that the base requirements have been met
+for the graders.
+
+
+To test the 5 additional queries required, look at the "Query description"
+section in the "Program4_DatabaseDesign.pdf" file in the doc folder. The pages/views
+that implement those queries are shown in each query description. Go to those pages
+in the application using a user that has the appropriate type in order to see confirmation that
+they work.
+
+
+Work-Load Distribution
+-----------------
+Alex: JSP pages (Employee, Customer), DatabaseController.java, database design document
+Bijan: JSP pages (Manager), DatabaseController.java, setup.sql, database design document
+Colin: DatabaseController.java, setup.sql, database design document + queries
+Monica: DatabaseController.java, database design document + queries + ER diagram, documentation
+
+
+Very even work-load distribution amongst group members (approx. 25% for each
+group member).
+
--- ROOT/WEB-INF/classes/dbController/DatabaseController.java
+++ ROOT/WEB-INF/classes/dbController/DatabaseController.java
@@ -11,6 +11,39 @@ import java.util.Scanner;
 import java.util.Vector;
 import java.util.Calendar;
 
+/*=============================================================================
+|   Assignment:  Program #4:  Database-driven Web Application
+|       Author:  Alex Yee, Bijan Anjavi, Colin Widner, Monica Sproul
+|       Grader:  Jacob Combs and/or Yawen Chen
+|
+|       Course:  CSC 460
+|   Instructor:  L. McCann
+|     Due Date:  May 1st, 2017, at the beginning of class (we are using our 1 late day)
+|
+|  Run/Compile:  Run and compile this program on lectura in the tomcat folder. Instructions on how to use
+	 Tomcat in lectura are described in the HowTo.txt posted in the CSC460 directory on lectura.
+
+	 Important steps when running this application:
+	 -Remember to set the environment variables
+	 -Restart server whenever compiling DatabaseCollection.java
+	 -Compile DatabaseCollection.java with the following command: > /usr/local/jdk/bin/javac DatabaseController.java
+|
+|  Description:  "An interactive database-driven web information management system from
+ground up. We will give you an application domain to work on, your goal is to design the underlying database
+and define the application functionalities you will provide with the database, and lastly implement this application
+as a web-based system" from project specification. Successive output and completion of queries is shown relative to what the user inputs according to the prompt above.
+
+	Techniques: The scrubbing and data conversion process is described in depth in README.txt. In order to accomplish the main goal of Prog3, which
+				was create an interactive menu that allows a user to query the database, the use of JDBC was required. Via the tutorial in the sample
+				program JDBC.java provided by the instructor, JDBC was implemented and the user's responses were able to be interpreted in a way such that
+				JDBC and Oracle interacted to present the results of that query back to the user (for whatever query they selected).
+
+|     Language:  ***Java (JDK 1.8) (MAKE SURE TO COMPILE WITH 1.7 AS DESCRIBED IN HOWTO.TXT)
+| Ex. Packages:  java.io, java.util
+|
+| Deficiencies:  The requirements have all been met as far as I know.
+	Known Bugs:  None; program operates as expected.
+*===========================================================================*/
 public class DatabaseController {
 	static final long serialVersionUID = 1L;
 	  /**
@@ -35,6 +68,7 @@ public class DatabaseController {
 	   */
 	  protected String username = null;
 
+ // GIVEN CONSTRUCTOR
 	public DatabaseController() {
 	    // your cs login name
 	    username = "banjavi"; // need owner of table for insertions
@@ -44,7 +78,7 @@ public class DatabaseController {
 	  }
 
 
-	/**
+	/** GIVEN METHOD
 	   * Closes the DBMS connection that was opened by the open call.
 	   */
 	  public void Close() {
@@ -58,8 +92,7 @@ public class DatabaseController {
 	    connection_ = null;
 	  }
 
-
-	  /**
+	  /** GIVEN METHOD
 	   * Commits all update operations made to the dbms.
 	   * If auto-commit is on, which is by default, it is not necessary to call
 	   * this method.
@@ -74,6 +107,10 @@ public class DatabaseController {
 	    }
 	  }
 
+		/** GIVEN METHOD
+	   * Open connection to the DBMS that will allow for queries to be sent.
+	   * This method must be called whenever using methods from this class.
+	   */
 	  public void Open() {
 	    try {
 	        Class.forName("oracle.jdbc.OracleDriver");
@@ -92,7 +129,21 @@ public class DatabaseController {
 	    }
 	    }
 
-		// there should either be 1 or 0 tuples returned - we need to prevent duplicate users later on
+
+	/*---------------------------------------------------------------------
+    |  Method authenticate
+    |
+    |  Purpose: Authneticate a user when they trie to login at the home menu.
+    |
+    |  Pre-condition: The user has to submit a potential username and password Sring.
+    |
+    |  Post-condition: The potential username and password has been authenticated.
+    |
+    |  Parameters:
+    |      username, password (String) -- the username and password submitted for authentication
+    |
+    |  Returns:  String - return error, or a string containing information about that user
+    *-------------------------------------------------------------------*/
 		public String authenticate (String username, String password) {
 			String sql_query = "SELECT user_id, username, type FROM banjavi.users WHERE username= '" + username + "' AND password= '" + password + "'";
 			try {
@@ -109,7 +160,20 @@ public class DatabaseController {
 			}
 			return "error";
 		}
-
+		/*---------------------------------------------------------------------
+	    |  Method FindAllUsers
+	    |
+	    |  Purpose: Display all the users according to the type requested by a Manager.
+	    |
+	    |  Pre-condition: A manager is logged in and is request to view the User Settings page.
+	    |
+	    |  Post-condition: The users have been displayed to the manager according to the filter type.
+	    |
+	    |  Parameters:
+	    |      filterType (String) -- searching for results with this value in the type field
+	    |
+	    |  Returns:  Vector<String> -contains the result of the SQL query in str format
+	    *-------------------------------------------------------------------*/
 		public Vector<String> FindAllUsers(String filterType) {
 			String sql_query="";
 			if(filterType.compareTo("All") == 0){
@@ -133,7 +197,21 @@ public class DatabaseController {
 			}
 			return null;
 		}
-
+	/*---------------------------------------------------------------------
+		|  Method insertUser
+		|
+		|  Purpose: Add a new user to the Users table.
+		|  Pre-condition: Manager has to be logged in and submitted a request to add a new user.
+		|
+		|  Post-condition: A new user has been added to the table and the manager can see this.
+		|
+		|  Parameters:
+		|      username (String) -- self-explanatory
+		|      password (String) -- self-explanatory
+		|      type (String) -- type of that user (Employee, Manager, or Customer)
+		|
+		|  Returns:  boolean indicating whether the operation was successful
+		*-------------------------------------------------------------------*/
 	public boolean insertUser (String username, String password, String type) {
 
 
@@ -147,8 +225,21 @@ public class DatabaseController {
 			return false;
 		}
 
+	/*---------------------------------------------------------------------
+		|  Method deleteUser
+		|
+		|  Purpose: Delete a user from the Users table.
+		|  Pre-condition: Manager has to be logged in and submitted a request to delete a user.
+		|
+		|  Post-condition: A user has been deleted from the table and the manager can see this.
+		|
+		|  Parameters:
+		|      id (String) -- id of the user to be deleted
+		|
+		|  Returns:  boolean indicating whether the operation was successful
+		*-------------------------------------------------------------------*/
 		public boolean deleteUser (String id) {
-			String sql_query1 = 	"delete from banjavi.orders where user_id=" + id;
+			String sql_query1 = 	"delete from banjavi.orders where user_id=" + id; // IMPORTANT -- delete user_id from Orders because of the PK-FK connection
 			String sql_query2 = 	"delete from banjavi.users where user_id=" + id;
 			try {
 				ResultSet rs1  = statement_.executeQuery(sql_query1);
@@ -160,6 +251,20 @@ public class DatabaseController {
 			return false;
 		}
 
+		/*---------------------------------------------------------------------
+			|  Method updateUser
+			|
+			|  Purpose: Update the category of an existing user in the Users table.
+			|  Pre-condition: Manager has to be logged in and submitted a request to update a user.
+			|
+			|  Post-condition: A type for a user has been updated and the manager can see this.
+			|
+			|  Parameters:
+			|      username (String) -- self-explanatory
+			|      newType (String) -- the new type of that user (Employee, Manager, or Customer)
+			|
+			|  Returns:  boolean indicating whether the operation was successful
+			*-------------------------------------------------------------------*/
     public boolean updateUser (String username, String newType) {
 
 			String sql_query = 	"update banjavi.users set type='"+ newType + "'where username='" + username +"'";
@@ -174,6 +279,20 @@ public class DatabaseController {
 
 
 		//groups by category
+		/*---------------------------------------------------------------------
+	    |  Method FindAllProducts
+	    |
+	    |  Purpose: Display all the products according to the category requested by a Manager.
+	    |
+	    |  Pre-condition: A manager/employee is logged in and is request to view the Stock Info page.
+	    |
+	    |  Post-condition: The products have been displayed according to the filter category.
+	    |
+	    |  Parameters:
+	    |      filterCategory (String) -- searching for results with this value in the category field
+	    |
+	    |  Returns:  Vector<String> -contains the result of the SQL query in  vector of str format
+	    *-------------------------------------------------------------------*/
 	  public Vector<Vector<String>> FindAllProducts(String filterCategory) {
 			String sql_query="";
 			if(filterCategory.compareTo("All") == 0){
@@ -207,7 +326,20 @@ public class DatabaseController {
 	    return null;
 	  }
 
+		/*---------------------------------------------------------------------
+	    |  Method viewCustomerOrders
+	    |
+	    |  Purpose: Display all the past customer orders for the logged-in customer.
+	    |
+	    |  Pre-condition: The view for this query must be pulled up by the loggedin User.
+	    |
+	    |  Post-condition: The output of the SQL query has been displayed to the User in the form a .jsp page.
+	    |
+	    |  Parameters:
+	    |      user_id (String) --the user_id of the customer in question
 
+	    |  Returns:  Vector<String> -contains the result of the SQL query in str format
+	    *-------------------------------------------------------------------*/
 		public Vector<String> viewCustomerOrders(String user_id) {
 			String sql_query = "SELECT order_id, date_placed, pick_up_date, name, quantity, price * CAST(quantity AS number(*,2)) AS \"COST\" FROM banjavi.orders, banjavi.products WHERE banjavi.orders.product_id = banjavi.products.product_id AND user_id = " + user_id + " ORDER BY order_id";
 			try {
@@ -228,6 +360,21 @@ public class DatabaseController {
 	    return null;
 
 	}
+
+	/*---------------------------------------------------------------------
+		|  Method TotalCost
+		|
+		|  Purpose: Display the total cost for each ofthe past customer orders for the logged-in customer.
+		|
+		|  Pre-condition: The view for this query must be pulled up by the loggedin User.
+		|
+		|  Post-condition: The output of the SQL query has been displayed to the User in the form a .jsp page.
+		|
+		|  Parameters:
+		|      order_id (String) --the order_id of the customer's order in question
+
+		|  Returns:  double - the summed total cost for an order
+		*-------------------------------------------------------------------*/
 	public double TotalCost(int orderID){
 	String sql_query = "SELECT SUM(price * CAST(quantity AS number(*,2))) AS \"TOTALCOST\" from banjavi.orders, banjavi.products where banjavi.orders.product_id = banjavi.products.product_id and order_id="+ orderID;
 	// SELECT SUM(price * CAST(quantity AS number(*,2))) AS "TOTALCOST" from banjavi.orders, banjavi.products where banjavi.orders.product_id = banjavi.products.product_id and order_id=3;
@@ -244,6 +391,20 @@ public class DatabaseController {
 }
 
 // type is either Manager or Customer
+/*---------------------------------------------------------------------
+	|  Method viewAllOrders
+	|
+	|  Purpose: Display all of the orders for a Manager or Employee to check out (check user_id to see whether it is a supply order from a manager, or customer order)
+	|
+	|  Pre-condition: The view for this query must be pulled up by the loggedin User.
+	|
+	|  Post-condition: The output of the SQL query has been displayed to the User in the form a .jsp page.
+	|
+	|  Parameters:
+	|      type (String) --the user type corresponding to type of order (Manager -> supply, and Customer -> online)
+
+	|  Returns:  Vector<String> -contains the result of the SQL query in str format
+	*-------------------------------------------------------------------*/
 	public Vector<String> viewAllOrders(String type) {
 		String sql_query = "SELECT order_id, banjavi.orders.user_id, date_placed, pick_up_date, product_id, quantity FROM banjavi.orders, banjavi.users WHERE banjavi.orders.user_id = banjavi.users.user_id AND type = '" + type + "' ORDER BY order_id";
 		try {
@@ -265,6 +426,23 @@ public class DatabaseController {
 
 }
 
+/*---------------------------------------------------------------------
+	|  Method orderProducts
+	|
+	|  Purpose: Allow a user to order a product (can be for a supply order or a regular customer order).
+	|
+	|  Pre-condition: The view for this query must be pulled up by the loggedin User.
+	|
+	|  Post-condition: The output of the SQL query has been displayed to the User in the form a .jsp page.
+	|
+	|  Parameters:
+	|      user_id (int) -- the user_id from order in question
+	|      product_id (int) --the product_id from the purchase in the order in question
+	|      quantity (int) -- the quantity for a product from order in question
+	|      order_id (int) --the order_id of the customer's order in question
+
+	|  Returns:  int - the order_id of the customer's order in question
+	*-------------------------------------------------------------------*/
 	public int orderProducts(int userID, int productID, int quantity, int orderID) {
 			int oID = 0;
 			String type = "";
@@ -316,6 +494,21 @@ public class DatabaseController {
 		return oID;
 		}
 
+		/*---------------------------------------------------------------------
+			|  Method updateStock
+			|
+			|  Purpose: The stock will be updated when a supply order is made or a customer order is checked out.
+			|
+			|  Pre-condition: The view for this query must be pulled up by the loggedin User.
+			|
+			|  Post-condition: The output of the SQL query has been displayed to the User in the form a .jsp page.
+			|
+			|  Parameters:
+			|      product_id (int) --the product_id from the purchase in the order in question
+			|      qty (int) -- the quantity for a product from order in question
+
+			|  Returns:  void
+			*-------------------------------------------------------------------*/
 	public  void UpdateStock(int productID, int qty) {
 		 String query = "select stock from products where product_id = "+ productID;
 		 try{
@@ -335,6 +528,19 @@ public class DatabaseController {
 
 		  }
 
+			/*---------------------------------------------------------------------
+				|  Method viewUnprocessedOrders
+				|
+				|  Purpose: Display all of the unprocessed customer orders for a manager or employee to check out.
+				|
+				|  Pre-condition: The view for this query must be pulled up by the loggedin User.
+				|
+				|  Post-condition: The output of the SQL query has been displayed to the User in the form a .jsp page.
+				|
+				|  Parameters:
+
+				|  Returns:  Vector<String> -contains the result of the SQL query in str format
+				*-------------------------------------------------------------------*/
 	public Vector<String> viewUnprocessedOrders() {
 	@SuppressWarnings("deprecation")
 	java.sql.Date d = new java.sql.Date(0, 0, 0);
@@ -356,6 +562,20 @@ public class DatabaseController {
 	return null;
 	}
 
+	/*---------------------------------------------------------------------
+		|  Method checkout
+		|
+		|  Purpose: Manager or Employee can check out an unprocessed order for a user and confirm a pickup date.
+		|
+		|  Pre-condition: The view for this query must be pulled up by the loggedin User.
+		|
+		|  Post-condition: The output of the SQL query has been displayed to the User in the form a .jsp page.
+		|
+		|  Parameters:
+		|      orderID (int) --the orderID for the unrpocessed order
+
+		|  Returns: void
+		*-------------------------------------------------------------------*/
 	public void checkout(int orderID) {
 
 		String sql_query = "SELECT product_id, quantity FROM banjavi.orders WHERE order_id= " + orderID;
@@ -389,6 +609,20 @@ public class DatabaseController {
 		}
 	}
 
+	/*---------------------------------------------------------------------
+		|  Method ProductsToOrder
+		|
+		|  Purpose: returns the list of all products with stock less than the inputed number
+		|
+		|  Pre-condition:  The view for this query must be pulled up by the loggedin User.
+		|
+		|  Post-condition: The output of the SQL query has been displayed to the User in the form a .jsp page.
+		|
+		|  Parameters:
+		|      number (int) -- user inputted number
+		|
+		|  Returns: Vector Vector<String> -contains the result of the SQL query in  vector of str format
+		*-------------------------------------------------------------------*/
 	 public Vector<Vector<String>> ProductsToOrder(int number){
                 //returns the list of all products with stock less than the inputed number
                 String sql_query = "SELECT * FROM banjavi.products WHERE stock < " + number;
@@ -416,6 +650,20 @@ public class DatabaseController {
             return null;
           }
 
+					/*---------------------------------------------------------------------
+						|  Method OrdersContaingProduct
+						|
+						|  Purpose: returns a list of all orderIds containing the product
+						|
+						|  Pre-condition:  The view for this query must be pulled up by the loggedin User.
+						|
+						|  Post-condition: The output of the SQL query has been displayed to the User in the form a .jsp page.
+						|
+						|  Parameters:
+						|      productID (int) -- user inputted barcode
+						|
+						|  Returns:  Vector<Integer> -contains the result of the SQL query in  vector of str format
+						*-------------------------------------------------------------------*/
 	 public Vector<Integer> OrdersContainingProduct(int productID){
                 //returns a list of all orderIds containing the product
                 String sql_query = "SELECT order_id FROM banjavi.orders WHERE product_id = " + productID;
